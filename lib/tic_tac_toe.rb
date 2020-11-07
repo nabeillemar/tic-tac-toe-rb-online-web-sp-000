f11c4aa2d761a312d3bb86733dec28944451fc3a
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-07 01:14:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,6 +11,26 @@
 
 
 
+
+def turn_count(board)
+  counter = 0 
+  board.each do |token|
+  if token == "X" || token == "O"
+    counter += 1 
+    end 
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+  return "X"
+else 
+  return "O"
+  end
+end 
+  
+
 WIN_COMBINATIONS = [
   [0,1,2], #Top Row
   [3,4,5], # Middle Row 
