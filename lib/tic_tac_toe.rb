f86247f701ded9810d6cebdb87a3b447d65217e8
Nabeil Lemar <nabeillemar@gmail.com>
2020-11-07 01:26:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,3 +13,19 @@ end
 def player_move(board, index, marker)
  board[index] = marker
 end
+
+def position_taken? (board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
